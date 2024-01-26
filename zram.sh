cb4adf295b1a58a033d98ed69de875f3a3ec9002
Nicolas Cura <59953189+NCura@users.noreply.github.com>
2024-01-26 19:37:26 +0100

    Update zram.sh

--- zram.sh
+++ zram.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-red='\033[0;31m'
 
 sudo nala install zram-tools
 
@@ -9,5 +8,6 @@ sudo sed -i 's/# *ALGO=lz4/ALGO=lz4/' /etc/default/zramswap
 # Uncomment PERCENT=x and change to PERCENT=10 (regardless of the space and original value)
 sudo sed -i 's/# *PERCENT=.*/PERCENT=10/' /etc/default/zramswap
 
-
-printf ${red}"Please REBOOT\n"
+RED='\033[0;31m'
+NC='\033[0m'
+echo -e "${RED}Please reboot your system to apply changes.${NC}"
