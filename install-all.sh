d74b8d1f4b2526d98508515ee9673a75ce7dc118
Nicolas Cura <59953189+NCura@users.noreply.github.com>
2024-01-27 18:30:12 +0100

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -3,13 +3,6 @@
 # Change to the directory containing the scripts
 cd ~/.local/share/ncura/debian-install-scripts
 
-# Make each script executable
-chmod +x common-packages.sh
-chmod +x qtile.sh
-chmod +x zram.sh
-chmod +x flatpak.sh
-chmod +x nvidia.sh
-
 # Execute each script in turn
 ./common-packages.sh
 ./qtile.sh
